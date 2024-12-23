ba2de5afbbb1aed770e942947464a7e48d8711b9
adrianpiecek <adrian.piecek@gmail.com>
2024-12-23 21:37:47 +0100

    changed movement logic to vector based

--- ZombieStrike.py
+++ ZombieStrike.py
@@ -2,6 +2,7 @@ import pygame
 from random import randint
 import spriteSheet
 from itertools import cycle
+import math
 
 # Inicjalizacja Pygame
 pygame.init()
@@ -42,36 +43,37 @@ class Player(pygame.sprite.Sprite):
 
         self.speed = 3
         self.direction = 1
+        movement = pygame.Vector2(0, 0)
 
     def update(self):
         keys = pygame.key.get_pressed()
         self.image_index += self.animation_speed
-        if keys[pygame.K_w] or keys[pygame.K_s] or keys[pygame.K_a] or keys[pygame.K_d]:
+
+        if self.movement.length() > 0:
+            self.movement.normalize_ip()
+            self.rect.move_ip(self.movement * self.speed)
             if self.image_index >= len(self.walk):
                 self.image_index = 0
             self.image = self.walk[int(self.image_index)] if self.direction == 1 else pygame.transform.flip(self.walk[int(self.image_index)], True, False).convert_alpha()
-
         else:
             if self.image_index >= len(self.idle):
                 self.image_index = 0
             self.image = self.idle[int(self.image_index)] if self.direction == 1 else pygame.transform.flip(self.idle[int(self.image_index)], True, False).convert_alpha()
-        
 
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            self.rect.y -= self.speed
+            self.movement.y = -1
         if keys[pygame.K_s]:
-            self.rect.y += self.speed
+            self.movement.y = 1
         if keys[pygame.K_a]:
-            self.rect.x -= self.speed
+            self.movement.x = -1
             self.direction = -1
         if keys[pygame.K_d]:
-            self.rect.x += self.speed
+            self.movement.x = 1
             self.direction = 1
-
-
-
+        if not keys[pygame.K_w] and not keys[pygame.K_s] and not keys[pygame.K_a] and not keys[pygame.K_d]:
+            self.movement = pygame.Vector2(0, 0)
 #player sprite
 player = pygame.sprite.GroupSingle(Player())
 #ground tile
