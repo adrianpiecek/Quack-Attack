2cbc59ae2d0c5bd84a8ba49a0a382551a911495e
adrianpiecek <adrian.piecek@gmail.com>
2024-12-23 02:41:16 +0100

    check if player want to quit

--- ZombieStrike.py
+++ ZombieStrike.py
@@ -16,3 +16,11 @@ GREEN = (0, 255, 0)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Zombie Strike")
 clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    pygame.display.update()
